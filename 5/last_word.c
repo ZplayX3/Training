6e35cc696c00f62cd7332f108b4730593c6c29e6
ZplayX3 <84452548+ZplayX3@users.noreply.github.com>
2024-09-14 12:10:54 +0200

    Update last_word.c

--- 5/last_word.c
+++ 5/last_word.c
@@ -28,14 +28,18 @@ int	main(int argc, char **argv)
 		return (0);
 	}
 	i = 0;
+	//deplacement jusqu a la fin de la chaine
 	while (argv[1][i])
 		i++;
 	i--;
+	//reculer jusqu a la fin du dernier mot en cas de whitespaces
 	while (argv[1][i] && (argv[1][i] == ' ' || argv[1][i] == '\t'))
 		i--;
+	//reculer jusqu au debut du dernier mot
 	while (argv[1][i] && (argv[1][i] != ' ' && argv[1][i] != '\t'))
 		i--;
 	i++;
+	//repasser sur le mot et afficher les caracteres
 	while (argv[1][i] && (argv[1][i] != ' ' && argv[1][i] != '\t'))
 	{
 		ft_putchar(argv[1][i]);
