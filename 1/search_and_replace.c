ca77aee9959ad455f03a2d98a112e64fdc2bf6aa
ZplayX3 <84452548+ZplayX3@users.noreply.github.com>
2024-09-14 12:13:52 +0200

    Update search_and_replace.c

--- 1/search_and_replace.c
+++ 1/search_and_replace.c
@@ -10,43 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 /*
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+	Assignment name  : search_and_replace
+Expected files   : search_and_replace.c
+Allowed functions: write, exit
+--------------------------------------------------------------------------------
 
-void	search_and_replace(char src, char search, char replace)
-{
-	if (src == search)
-		ft_putchar(replace);
-	else
-		ft_putchar(src);
-}
+Write a program called search_and_replace that takes 3 arguments, the first
+arguments is a string in which to replace a letter (2nd argument) by
+another one (3rd argument).
 
-int	main(int argc, char **argv)
-{
-	char	search;
-	char	replace;
-	char	*str;
-	
-	search = (char)argv[2];
-	replace = (char)argv[3];
-	str = argv[1];
-	if (argc != 4)
-	{
-		ft_putchar('\n');
-		return (0);
-	}
-	while (*str)
-	{
-		search_and_replace(*str, search, replace);
-		str++;
-	}
-	ft_putchar('\n');
-}
-*/
+If the number of arguments is not 3, just display a newline.
+
+If the second argument is not contained in the first one (the string)
+then the program simply rewrites the string followed by a newline.
+
+Examples:
+$>./search_and_replace "Papache est un sabre" "a" "o"
+Popoche est un sobre
+$>./search_and_replace "zaz" "art" "zul" | cat -e
+$
+$>./search_and_replace "zaz" "r" "u" | cat -e
+zaz$
+$>./search_and_replace "jacob" "a" "b" "c" "e" | cat -e
+$
+$>./search_and_replace "ZoZ eT Dovid oiME le METol." "o" "a" | cat -e
+ZaZ eT David aiME le METal.$
+$>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
+eNcOre Un ExEmPle Pas Facile a Ecrire $
+#include <unistd.h>
+	*/
+
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
